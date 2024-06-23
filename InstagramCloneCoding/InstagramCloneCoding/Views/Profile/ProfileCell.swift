ff11d42ccbc5223b4c52a2f27d0880388d00fca0
won <andrew112517@naver.com>
2024-06-23 18:01:10 +0900

    [Feat]

--- InstagramCloneCoding/InstagramCloneCoding/Views/Profile/ProfileCell.swift
+++ InstagramCloneCoding/InstagramCloneCoding/Views/Profile/ProfileCell.swift
@@ -10,12 +10,32 @@ import UIKit
 class ProfileCell: UICollectionViewCell {
     
     // MARK: - Properties
+    private let postImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(named: "venom-7")
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
     
     // MARK: - Life Cycle ⭐️
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = .lightGray
+        setupUI()
+    }
+    
+    // MARK: - Set up UI
+    func setupUI() {
+        self.backgroundColor = .systemGray
+        self.addSubview(postImageView)
+        
+        setupConstraints()
+    }
+    
+    // MARK: - Set up Constraints
+    func setupConstraints() {
+        postImageView.fillSuperview()
     }
     
     required init?(coder: NSCoder) {
